350908433c76c13d96dd154ee1ecfee6967a305d
DiBez1115 <dimabezuglov777@gmail.com>
2023-04-03 08:57:03 +0300

    Переделал 3 лабораторную работу в соответствии с требованиями

--- student_27/BUKEP.Student.Calculator/BUKEP.Student.Calculator/Program.cs
+++ student_27/BUKEP.Student.Calculator/BUKEP.Student.Calculator/Program.cs
@@ -7,62 +7,82 @@ namespace BUKEP.Student.Calculator
     {
         static void Main(string[] args)
         {
-        Console.WriteLine("Введите выражение состоящие из двух значений, между значениями должен быть пробел");
-        var InpuеData = Console.ReadLine().Split(' ');
-        var elements = new List<object>();
-
-            for (int a = 0; a < InpuеData.Length; a++)            
-                if (InpuеData[a] != "")                
-                    elements.Add(InpuеData[a]);
-            
-            for (int i = 0; i < elements.Count; i++)
-        {
-            switch (elements[i])
+            while (true)
             {
-                case "+":
-                    var n1 = Convert.ToDouble(elements[elements.IndexOf("+") - 1]);
-                    var n2 = Convert.ToDouble(elements[elements.IndexOf("+") + 1]);
-                    double AddNum = n1 + n2;
-                    elements.Add("= " + AddNum);
-                    Print(elements);
-                    break;
+                Console.Clear();
+                Console.WriteLine("Введите простое математическое выражение для вычисления операции (+ или - или * или /) над двумя числами. По завершению ввода операции нажмите Enter:");
+                var InpuеData = Console.ReadLine();
+                var result = InpuеData.Split(new char[] { '+', '-', '/', '*' });
+                var elements = new List<object>();
+                for (int a1 = 0; a1 < result.Length; a1++)
+                {
+                    var result1 = result[a1].Trim();
+                    elements.Add(result1);
+                }
 
-                case "-":
-                    var n3 = Convert.ToDouble(elements[elements.IndexOf("-") - 1]);
-                    var n4 = Convert.ToDouble(elements[elements.IndexOf("-") + 1]);
-                    AddNum = n3 - n4;
-                    elements.Add("= " + AddNum);
-                    Print(elements);
-                    break;
+                if (InpuеData.Contains("+")) elements.Insert(1, "+");
+                if (InpuеData.Contains("-")) elements.Insert(1, "-");
+                if (InpuеData.Contains("/")) elements.Insert(1, "/");
+                if (InpuеData.Contains("*")) elements.Insert(1, "*");
 
-                case "*":
-                    var n5 = Convert.ToDouble(elements[elements.IndexOf("*") - 1]);
-                    var n6 = Convert.ToDouble(elements[elements.IndexOf("*") + 1]);
-                    AddNum = n5 * n6;
-                    elements.Add("= " + AddNum);
-                    Print(elements);
-                    break;
-
-                case "/":
-                    var n7 = Convert.ToDouble(elements[elements.IndexOf("/") - 1]);
-                    var n8 = Convert.ToDouble(elements[elements.IndexOf("/") + 1]);
-                    if (n8 != 0)
+                for (int i = 0; i < elements.Count; i++)
+                {
+                    switch (elements[i])
                     {
-                        AddNum = n7 / n8;
-                        elements.Add("= " + AddNum);
-                        Print(elements);
-                    }
-                    else
-                        Console.WriteLine("На ноль делить нельзя");
-                    break;
-                    }
-            }
+                        case "+":
+                            var n1 = Convert.ToDouble(elements[elements.IndexOf("+") - 1]);
+                            var n2 = Convert.ToDouble(elements[elements.IndexOf("+") + 1]);
+                            double AddNum = n1 + n2;
+                            elements.Add("= " + AddNum);
+                            Print(elements);
+                            Console.Clear();
+                            break;
 
+                        case "-":
+                            var n3 = Convert.ToDouble(elements[elements.IndexOf("-") - 1]);
+                            var n4 = Convert.ToDouble(elements[elements.IndexOf("-") + 1]);
+                            AddNum = n3 - n4;
+                            elements.Add("= " + AddNum);
+                            Print(elements);
+                            Console.Clear();
+                            break;
+
+                        case "*":
+                            var n5 = Convert.ToDouble(elements[elements.IndexOf("*") - 1]);
+                            var n6 = Convert.ToDouble(elements[elements.IndexOf("*") + 1]);
+                            AddNum = n5 * n6;
+                            elements.Add("= " + AddNum);
+                            Print(elements);
+                            Console.Clear();
+                            break;
+
+                        case "/":
+                            var n7 = Convert.ToDouble(elements[elements.IndexOf("/") - 1]);
+                            var n8 = Convert.ToDouble(elements[elements.IndexOf("/") + 1]);
+                            if (n8 != 0)
+                            {
+                                AddNum = n7 / n8;
+                                elements.Add("= " + AddNum);
+                                Print(elements);
+                            }
+                            else
+                                Console.WriteLine("На ноль делить нельзя");
+                            Console.ReadKey();
+                            Console.Clear();                            
+                            break;
+                    }
+                }                
+                Console.Write("Для повторного ввода операции нажмите Enter, для завершения приложения Esc.");
+                
+                    if (Console.ReadKey().Key == ConsoleKey.Escape) { break; }                                              
+            }
         }
         static void Print(List<object> elems)
         {
-             for (int a = 0; a < elems.Count; a++)
-                Console.Write(elems[a] + " ");
+            for (int a = 0; a < elems.Count; a++)
+                Console.Write(elems[a] + " ");               
+            Console.ReadKey();
+            Console.Clear();
         }
     }
 }
