f7995370b2b5a6711f6fbe43c598a7d379a05b49
DiBez1115 <dimabezuglov777@gmail.com>
2023-05-05 16:45:27 +0300

    Выполнил лабораторную работу №7

---
+++ student_27/BUKEP.Student.ConsoleCalculator/BUKEP.Student.ConsoleCalculator/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using BUKEP.Student.Calculator;
+
+namespace BUKEP.Student.ConsoleCalculator
+{
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+
+                Console.Write("Введите математическое выражение: ");
+
+                string line = Console.ReadLine();
+                
+                if (CalculatingExpressions.ChecksExpressionsForLetters(line) == false)
+                {
+                    string expression = "";
+
+                    foreach (char symbol in line)
+                    {
+                        if (symbol != ' ')
+                        {
+                            expression += symbol;
+                        }
+
+                    }
+
+                    Console.Write("Результат: "+ expression + "=" + CalculatingExpressions.PassTheExpressionToCalculatingExpressions(expression));
+
+                    Console.ReadKey();
+
+                    Console.Clear();
+                }
+
+                else
+                {
+                    Console.WriteLine("Ошибка!!! Не удалось распознать выражение!");
+
+                    Console.ReadKey();
+
+                    Console.Clear();
+                }
+
+                Console.Write("Для повторного ввода операции нажмите Enter, для завершения приложения Esc.");
+
+                if (Console.ReadKey().Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+
+            }
+
+        }
+
+    }
+
+}
