404db04b2dbcb3411916e2ca9f13cbab0cc81729
morren <chilleam.biz@gmail.com>
2023-05-06 21:42:12 +0300

    Реализовала калькулятор для лабораторной работы 3

---
+++ student_299/BUKEP.Student.ConsoleCalculator/ConsoleCalculator/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace BUKEP.Student.ConsoleCalculator
+{
+    internal class Program
+    {
+        public static ConsoleKey push_key = ConsoleKey.Enter;
+        static void Main(string[] args)
+        {
+            while(push_key == ConsoleKey.Escape)
+            {
+                Process.GetCurrentProcess().Kill();
+            }
+            while (push_key == ConsoleKey.Enter)
+            {
+                Console.Clear();
+                Calculate();
+            }
+
+        }
+
+        public static void Calculate()
+        {
+            Console.WriteLine("Введите простое математическое выражение для вычисления операции (+ или - или * или /) над двумя числами. По завершению ввода операции нажмите Enter:");
+            string expression = Console.ReadLine();
+            string[] operators = expression.Split(new char[] { '+', '-', '*', '/' });
+            int result;
+            for (int i = 0; i < expression.Length; i++)
+            {
+                switch (expression[i])
+                {
+                    case '+':
+                        result = Int32.Parse(operators[0]) + Int32.Parse(operators[1]);
+                        Console.WriteLine($"{operators[0]}{expression[i]}{operators[1]}={result}");
+                        Console.WriteLine("Для продолжения нажмите Enter для выхода нажмите любую клавишу на клавиатуре.");
+                        push_key = Console.ReadKey().Key;
+                        break;
+
+                    case '-':
+                        result = Int32.Parse(operators[0]) - Int32.Parse(operators[1]);
+                        Console.WriteLine($"{operators[0]}{expression[i]}{operators[1]}={result}");
+                        Console.WriteLine("Для продолжения нажмите Enter для выхода нажмите любую клавишу на клавиатуре.");
+                        push_key = Console.ReadKey().Key;
+                        break;
+
+                    case '*':
+                        result = Int32.Parse(operators[0]) * Int32.Parse(operators[1]);
+                        Console.WriteLine($"{operators[0]}{expression[i]}{operators[1]}={result}");
+                        Console.WriteLine("Для продолжения нажмите Enter для выхода нажмите любую клавишу на клавиатуре.");
+                        push_key = Console.ReadKey().Key;
+                        break;
+
+                    case '/':
+                        result = Int32.Parse(operators[0]) / Int32.Parse(operators[1]);
+                        Console.WriteLine($"{operators[0]}{expression[i]}{operators[1]}={result}");
+                        Console.WriteLine("Для продолжения нажмите Enter для выхода нажмите любую клавишу на клавиатуре.");
+                        push_key = Console.ReadKey().Key;
+                        break;
+                }
+            }   
+        }
+    }
+}
