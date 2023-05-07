57bfd092fb0de31714d2f14e1b74581a7639aafc
morren <chilleam.biz@gmail.com>
2023-05-07 12:03:02 +0300

    Исправлены ошибки в программе, добавлен функционал - проверки на некорректный ввод

--- student_299/BUKEP.Student.ConsoleCalculator/ConsoleCalculator/Program.cs
+++ student_299/BUKEP.Student.ConsoleCalculator/ConsoleCalculator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 
 namespace BUKEP.Student.ConsoleCalculator
 {
@@ -23,42 +24,81 @@ namespace BUKEP.Student.ConsoleCalculator
         public static void Calculate()
         {
             Console.WriteLine("Введите простое математическое выражение для вычисления операции (+ или - или * или /) над двумя числами. По завершению ввода операции нажмите Enter:");
+
+            //variables for main expression and find operators(a+b) and find type of operation(+, -, ...)
             string expression = Console.ReadLine();
             string[] operators = expression.Split(new char[] { '+', '-', '*', '/' });
-            int result;
-            for (int i = 0; i < expression.Length; i++)
+            char operation = '/';
+
+
+            foreach(string item in operators) //find type of operation
             {
-                switch (expression[i])
+                expression = expression.Replace(item, String.Empty);
+            }
+
+            operation = expression[0];
+
+            //variables for calculate
+            int number_1 = 0;
+            int number_2 = 0;
+            int result = 0;
+            
+            //variable for control errors
+            bool find_errors = false;
+
+            foreach(string item in operators) //check for numbers in input
+            {
+                if (item.All(char.IsDigit))
                 {
-                    case '+':
-                        result = Int32.Parse(operators[0]) + Int32.Parse(operators[1]);
-                        Console.WriteLine($"{operators[0]}{expression[i]}{operators[1]}={result}");
-                        Console.WriteLine("Для продолжения нажмите Enter для выхода нажмите любую клавишу на клавиатуре.");
-                        push_key = Console.ReadKey().Key;
-                        break;
-
-                    case '-':
-                        result = Int32.Parse(operators[0]) - Int32.Parse(operators[1]);
-                        Console.WriteLine($"{operators[0]}{expression[i]}{operators[1]}={result}");
-                        Console.WriteLine("Для продолжения нажмите Enter для выхода нажмите любую клавишу на клавиатуре.");
-                        push_key = Console.ReadKey().Key;
-                        break;
-
-                    case '*':
-                        result = Int32.Parse(operators[0]) * Int32.Parse(operators[1]);
-                        Console.WriteLine($"{operators[0]}{expression[i]}{operators[1]}={result}");
-                        Console.WriteLine("Для продолжения нажмите Enter для выхода нажмите любую клавишу на клавиатуре.");
-                        push_key = Console.ReadKey().Key;
-                        break;
-
-                    case '/':
-                        result = Int32.Parse(operators[0]) / Int32.Parse(operators[1]);
-                        Console.WriteLine($"{operators[0]}{expression[i]}{operators[1]}={result}");
-                        Console.WriteLine("Для продолжения нажмите Enter для выхода нажмите любую клавишу на клавиатуре.");
-                        push_key = Console.ReadKey().Key;
-                        break;
+                    number_1 = Int32.Parse(operators[0].ToString());
+                    number_2 = Int32.Parse(operators[1].ToString());
+                    break;
                 }
-            }   
+                else
+                {
+                    Console.WriteLine("Введены некорректные значения.");
+                    find_errors = true;
+                    break;
+                }
+            }
+
+            switch (operation)
+            {
+                case '+':
+                    result = number_1 + number_2;
+                    break;
+
+                case '-':
+                    result = number_1 - number_2; ;
+                    break;
+
+                case '*':
+                    result = number_1 * number_2; ;
+                    break;
+
+                case '/':
+                    if (number_2 == 0)
+                    {
+                        find_errors = true;
+                        Console.WriteLine("На ноль делить нельзя.");
+                        break;
+                    }
+                    else
+                    {
+                        result = number_1 / number_2; ;
+                        break;
+                    }
+                default:
+                    Console.WriteLine("Такая операция отсутствует.");
+                    break;
+            }
+
+            if (!find_errors)
+            {
+                Console.WriteLine($"{number_1}{operation}{number_2}={result}");
+            }
+            Console.Write("Для продолжения нажмите Enter для выхода нажмите любую клавишу на клавиатуре.");
+            push_key = Console.ReadKey().Key;
         }
     }
 }
